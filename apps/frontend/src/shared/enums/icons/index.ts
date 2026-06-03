221dd9780ac734a87f500b11c515d6fc614156b5
barabel <barabel324@yandex.ru>
2026-06-03 22:45:23 +0400

    fab button

--- apps/frontend/src/shared/enums/icons/index.ts
+++ apps/frontend/src/shared/enums/icons/index.ts
@@ -1,6 +1,8 @@
 export enum IconsArray {
   analytics = 'analytics',
   close = 'close',
+  fuel = 'fuel',
   list = 'list',
+  odometer = 'odometer',
   settings = 'settings',
 }
--- apps/frontend/src/shared/ui/fab-button/types/index.ts
+++ apps/frontend/src/shared/ui/fab-button/types/index.ts
@@ -1,8 +1,13 @@
+import type { IconsArray } from '@/shared/enums/icons';
+
 type TFabButtonItem = {
   title: string;
   value: string;
+  icon: IconsArray;
+  iconClassName: string;
 };
 
 export type TFabButton = {
   items: TFabButtonItem[];
+  onFabItemClick?: (itemValue: TFabButtonItem['value']) => void;
 };
